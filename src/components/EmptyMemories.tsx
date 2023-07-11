113ba1ce0bcc135e87feecd948c7e69e295f41b4
devLeoLucc <devLeonardoLucchesi@gmail.com>
2023-07-11 18:59:28 -0300

    v1.0 - all codes

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        You have not created any memories yet, start to{' '}
+        <a href="" className="underline hover:text-gray-50">
+          create now
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
